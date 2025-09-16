f736d7213f4aca565f7f381a9d931f2e5c05c3df
Akram <akrambenaoun1993@gmail.com>
2025-09-16 15:49:29 +0100

    remove comments

--- models/soil.models.js
+++ models/soil.models.js
@@ -1,18 +1,16 @@
 const mongoose = require("mongoose");
 
 const SoilSchema = new mongoose.Schema({
-  // Soil type
   soilType: {
     type: String,
     enum: ["sandy", "loamy", "clayey", "peaty", "chalky"],
     required: true,
   },
-  // Soil properties
   soilPH: {
     type: Number,
     min: 0,
     max: 14,
-    required: true, // acidic(0) → alkaline(14)
+    required: true,
   },
 });
 
